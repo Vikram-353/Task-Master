a14349a2ca663753a42d9cf7ede235d6359b4027
Vikram <vikramranjan71122@gmail.com>
2025-05-11 21:55:50 +0530

    charts

--- frontend/Task-Master/src/components/layouts/DashBoardLayout.jsx
+++ frontend/Task-Master/src/components/layouts/DashBoardLayout.jsx
@@ -12,7 +12,7 @@ function DashBoardLayout({ children, activeMenu }) {
     <div>
       <NavBar activeMenu={activeMenu} />
       {user && (
-        <div className="flex">
+        <div className="flex overflow-auto">
           <div className="max-[1080px]:hidden">
             <SideMenu activeMenu={activeMenu} />
           </div>
--- frontend/Task-Master/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Master/src/pages/Admin/Dashboard.jsx
@@ -11,6 +11,7 @@ import InfoCard from "../../components/Cards/InfoCard";
 import TaskListTable from "../../components/TaskListTable";
 import { LuArrowRight } from "react-icons/lu";
 import CustomPieChart from "../../components/Charts/CustomPieChart";
+import CustomBarChart from "../../components/Charts/CustomeBarChart";
 
 const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"];
 
@@ -130,6 +131,14 @@ function Dashboard() {
               label="Total Balance"
               colors={COLORS}
             ></CustomPieChart>
+          </div>
+        </div>
+        <div>
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">Task Priority Levels</h5>
+            </div>
+            <CustomBarChart data={barChartData}></CustomBarChart>
           </div>
         </div>
 
