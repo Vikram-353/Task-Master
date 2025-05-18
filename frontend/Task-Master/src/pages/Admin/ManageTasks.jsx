dac8c61ad4d97b027a0e3944e3a2f1f8425f74b7
Vikram <vikramranjan71122@gmail.com>
2025-05-18 13:11:03 +0530

    Manage Tasks

--- frontend/Task-Master/src/pages/Admin/ManageTasks.jsx
+++ frontend/Task-Master/src/pages/Admin/ManageTasks.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DashBoardLayout from "../../components/layouts/DashBoardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import TaskStatusTabs from "../../components/layouts/TaskStatusTabs";
+import TaskCard from "../../components/Charts/TaskCards";
 
 function ManageTasks() {
-  return <div>ManageTasks</div>;
+  const [allTasks, setAllTasks] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+  const navigate = useNavigate();
+  const getAllTasks = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: { status: filterStatus === "All" ? "" : filterStatus },
+      });
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        { label: "All", count: statusSummary.all || 0 },
+        { label: "Pending", count: statusSummary.pendingTasks || 0 },
+        { label: "In Progress", count: statusSummary.inProgressTasks || 0 },
+        { label: "Completed", count: statusSummary.CompletedTasks || 0 },
+      ];
+
+      setTabs(statusArray);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const handleClick = (taskData) => {
+    navigate(`/amdin/create-task`, { state: { taskId: taskData._id } });
+  };
+
+  const handleDownloadReport = async () => {};
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => {};
+  }, [filterStatus]);
+  return (
+    <DashBoardLayout activeMenu="Manage Tasks">
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+          <div className="flex items-center justify-between gap-3">
+            <h2 className="text-xl md:text-xl font-medium"> My Tasks</h2>
+
+            <button
+              className="flex lg:hidden download-btn"
+              onClick={handleDownloadReport}
+            >
+              <LuFileSpreadsheet className="text-lg"></LuFileSpreadsheet>
+              Download Report
+            </button>
+          </div>
+
+          {tabs?.[0]?.count > 0 && (
+            <div className="flex items-center gap-3">
+              <TaskStatusTabs
+                tabs={tabs}
+                activeTab={filterStatus}
+                setActiveTab={setFilterStatus}
+              ></TaskStatusTabs>
+
+              <button
+                className="hidden md:hidden download-btn"
+                onClick={handleDownloadReport}
+              >
+                <LuFileSpreadsheet className="text-lg"></LuFileSpreadsheet>
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allTasks?.map((item) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.title}
+              priority={item.priority}
+              progress={item.progress}
+              status={item.status}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => handleClick(item)}
+            ></TaskCard>
+          ))}
+        </div>
+      </div>
+    </DashBoardLayout>
+  );
 }
 
 export default ManageTasks;
